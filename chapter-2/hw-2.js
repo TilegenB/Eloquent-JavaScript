b30486dee0d2b16e60d0af0a862e18107c1f5902
TilegenB <tilegen.bakitov@outlook.com>
2022-06-06 17:43:51 +0500

    (chapt2-task2): done

--- chapter-2/hw-2.js
+++ chapter-2/hw-2.js
@@ -6,3 +6,14 @@ for (let i = 0; i < 8; i++) {
     console.log(result);
 }
 // 2 Task
+let num = 100;
+for (let i = 0; i < num; i++) {
+    if (i % 3 === 0) {
+        console.log('Fizz ' + [i]);
+    } else if (i % 5 === 0) {
+        console.log(`Buzz ${[i]}`);
+    } else if (i % 3 && i % 5) {
+        console.log(`FizzBuzz ${[i]}`);
+    }
+}
+// 3 Task
